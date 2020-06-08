0d7b44524078c1b3671974cc22944d84d71cf361
Jason <weible.8@osu.edu>
2020-06-07 20:38:49 -0400

    implemented abstract class and interface

---
+++ AEnum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fuckaround2
+{
+    public abstract class AEnum : IEnum
+    {
+        /* protected members are only accessible wihtin class and derived class */
+        public string Name { get; protected set; }
+        public int Value { get; protected set; }
+
+        protected ImageValues imageValues { get; private set; }
+
+        protected ImageList imageList { get; private set; }
+
+        public ImageValues ImageValues()
+        {
+            if (imageValues.X == -1 || imageValues.Y == -1 || imageValues.Width == -1 || imageValues.Height == -1)
+            {
+                // Should declare string message in String file so that enum is thrown with easily searchable error message
+                throw new InvalidOperationException("Image value not set, attempting to access undefined image");
+            }
+
+            return imageValues;
+        }
+
+        public ImageList ImageList()
+        {
+            if (imageList.IsEmpty())
+            {
+                // Should declare string message in String file so that enum is thrown with easily searchable error message
+                throw new InvalidOperationException("Image list is empty");
+            }
+
+            return imageList;
+        }
+
+        public void setFrame(ImageValues imVals)
+        {
+            imageValues = imVals;
+        }
+
+        public void setFrames(ImageList imList)
+        {
+            imageList = imList;
+        }
+
+      
+
+
+    }
+}
---
+++ IEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fuckaround2
+{
+    public interface IEnum
+    {
+
+        public ImageValues ImageValues();
+        public ImageList ImageList();
+        public void setFrame(ImageValues imVals);
+        public void setFrames(ImageList imList);
+       
+
+    }
+}
